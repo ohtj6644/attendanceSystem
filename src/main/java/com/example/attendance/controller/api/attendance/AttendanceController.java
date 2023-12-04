259e29217671a7bc9437f6f2b5d85d637c1bc67a
ohtj <ohtj@rmsoft.kr>
2023-12-04 16:52:56 +0900

    [병합 / 근무시작 js 연동]_004_front

--- src/main/java/com/example/attendance/controller/api/attendance/AttendanceController.java
+++ src/main/java/com/example/attendance/controller/api/attendance/AttendanceController.java
@@ -5,6 +5,7 @@ import com.example.attendance.entity.SiteUser;
 import com.example.attendance.service.AttendanceService;
 import com.example.attendance.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,18 +20,16 @@ public class AttendanceController {
 
     //------------------------근무 시작-------------------  //
     @GetMapping("/user/startWork")
-    public String startWork(Principal principal){
-        SiteUser user=userService.findUser(principal.getName());
-        // 이미 근무시작을 하였는지 확인.
-        if(attendanceService.getNowAttendance(user)!=null){
-            return "이미 오늘의 근무가 있습니다.";
+    public ResponseEntity<String> startWork(Principal principal) {
+        SiteUser user = userService.findUser(principal.getName());
 
-        // 근무시작을 안했을 경우 아래 메서드로 근무 생성
-        }else {
-            String workMessage= attendanceService.startWork(user);
-            return workMessage+" 근무 시작 ";
+        // 이미 근무 시작을 하였는지 확인
+        if (attendanceService.getNowAttendance(user) != null) {
+            return ResponseEntity.badRequest().body("이미 오늘의 근무가 있습니다.");
+        } else {
+            String workMessage = attendanceService.startWork(user);
+            return ResponseEntity.ok(workMessage + " 근무 시작");
         }
-
     }
 
     // ---------------------근무 종료-----------------------//
