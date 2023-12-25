62f5881953fbbb3161c45890cd6f6b1ab42bba53
Glen(TaekJoo OH) <126732472+ohtj6644@users.noreply.github.com>
2023-12-25 20:36:20 +0900

    Update my_attendance.js

--- src/main/resources/static/js/my_attendance.js
+++ src/main/resources/static/js/my_attendance.js
@@ -22,18 +22,29 @@ $(document).ready(function () {
                 //받아욘 Page 데이터를 updateTable 에 넘겨서 처리
                 alert("검색완료");
             })
-            .fail(function (error) {
-                console.error("오류:", error);
-            });
+            .fail(function (xhr, textStatus, errorThrown) {
+                        // HTTP 상태 코드가 400인 경우 오류 메시지 출력
+                        if (xhr.status === 400) {
+                            alert(xhr.responseText);
+                        } else {
+                            console.error("오류:", errorThrown);
+                        }
+                    });
     });
 
     function updateTable(page) {
-        var tableBody = $("tbody");
+        var tableBody = $("#attendanceBoard");
+        var countTableBody = $("#attendanceCount");
 
         // 기존 행 삭제
         tableBody.empty();
+        countTableBody.empty();
 
         if (page.content.length > 0) {
+
+            var totalWorkTime = 0; // 근무 시간 합 초기화
+            var totalRecords = page.totalElements; // 총 근무 기록 수
+
             // 받아온 page 데이터가 있을 때 테이블에 행 추가
             $.each(page.content, function (index, attendance) {
                 var jsDate1 = new Date(attendance.startWorkTime);
@@ -61,7 +72,16 @@ $(document).ready(function () {
                     "</tr>";
 
                 tableBody.append(row);
+                // 근무 시간 합 누적
+                totalWorkTime += attendance.workTime;
             });
+            // 총 근무 기록 수 및 근무 시간 합을 #attendanceCount 테이블에 추가
+                    var countRow = "<tr>" +
+                        "<td class='1'>" + totalRecords + "</td>" +
+                        "<td class='2'>" + parseInt(totalWorkTime / 60) + "시간" + totalWorkTime % 60 + "분" + "</td>" +
+                        "</tr>";
+
+                    countTableBody.append(countRow);
         } else {
             // 데이터가 없을 때 메시지 표시
             var messageRow = "<tr><td colspan='4'>해당 조회기간의 근무가 없습니다.</td></tr>";
