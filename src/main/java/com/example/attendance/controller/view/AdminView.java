81029d607e1f5e6de4a9f3693288b90120168d62
ohtj6644 <ohtj6644@naver.com>
2023-12-26 00:01:22 +0900

    [유저페이지 관리자버튼 권한부여]_otj

--- src/main/java/com/example/attendance/controller/view/AdminView.java
+++ src/main/java/com/example/attendance/controller/view/AdminView.java
@@ -4,6 +4,7 @@ import com.example.attendance.entity.SiteUser;
 import com.example.attendance.service.UserService;
 import com.example.attendance.user.UserCreateForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ public class AdminView {
     private final UserService userService;
     //----------------유저 회원가입 화면 반환 ------------------//
     @GetMapping("/admin")
+    @PreAuthorize("isAuthenticated()")
     public String signUp(UserCreateForm userCreateForm, Model model, Principal principal){
 
         SiteUser user = userService.findUser(principal.getName());
