a7600e8411a8489f6c675a29b2939a53f9a3c3e0
ohtj6644 <ohtj6644@naver.com>
2024-02-13 21:18:35 +0900

    [회사 문서][개발][관리자페이지 회사 문서 목록 구현 ]

--- src/main/java/com/example/attendance/controller/api/companyFile/CompanyFileController.java
+++ src/main/java/com/example/attendance/controller/api/companyFile/CompanyFileController.java
@@ -1,10 +1,111 @@
 package com.example.attendance.controller.api.companyFile;
 
 
+import com.example.attendance.entity.CompanyFile;
+import com.example.attendance.entity.SiteUser;
+import com.example.attendance.service.CompanyFileService;
+import com.example.attendance.service.FileService;
+import com.example.attendance.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.Principal;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 public class CompanyFileController {
+
+    private final UserService userService;
+
+    private final FileService fileService;
+
+    private final CompanyFileService companyFileService;
+
+
+    //--------------------회사 문서 추가---------------------//
+    @PostMapping("admin/companyFile/upload")
+    public ResponseEntity<String> companyFileUpload(@RequestPart("file") MultipartFile file, @RequestPart("text") String fileName, Principal principal) {
+
+        try {
+            // static/file/profile 경로 설정
+            Path uploadPath = Paths.get("static/file/companyFile/");
+
+            // 경로에 디렉토리가 존재하지 않으면 생성
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            String randomFileName = this.fileService.generateRandomFileName(12);
+
+            // 확장자를 포함한 새로운 파일 이름
+            String newFileName = randomFileName + this.fileService.getFileExtension(file.getOriginalFilename());
+
+
+            // 업로드할 파일의 경로를 설정
+            Path filePath = uploadPath.resolve(newFileName);
+
+            // 파일을 지정된 경로로 저장
+            Files.write(filePath, file.getBytes());
+
+            String fileUrl = "/file/companyFile/" + newFileName;
+            this.companyFileService.createCompanyFile(fileName, fileUrl);
+
+
+            // 성공적인 응답 반환
+            return ResponseEntity.ok("새 문서가 저장 되었습니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 에러 발생 시 실패 응답 반환
+            return ResponseEntity.badRequest().body("회서문서 등록오류 (관리자에게 문의 하세요)");
+        }
+    }
+
+
+    //--------------------회사 문서 삭제---------------------//
+    @GetMapping("admin/companyFile/delete/{id}")
+    public ResponseEntity<String> companyFileDelete(@PathVariable("id")String id){
+        CompanyFile companyFile = this.companyFileService.getCompanyFile(id);
+        String fileUrl = companyFile.getFileUrl();
+
+        if (fileUrl != null && !fileUrl.isEmpty()) {
+            try {
+                // 파일 경로를 사용하여 파일 객체 생성
+                File fileToDelete = new File(fileUrl);
+
+                // 파일이 존재하면 삭제
+                if (fileToDelete.exists()) {
+                    if (fileToDelete.delete()) {
+                        // 파일 삭제 성공
+                        this.companyFileService.companyFileDelete(companyFile);
+                        return ResponseEntity.ok("문서 및 파일이 삭제되었습니다.");
+                    } else {
+                        // 파일 삭제 실패
+                        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 삭제 실패");
+                    }
+                } else {
+                    // 파일이 존재하지 않음
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("파일을 찾을 수 없습니다.");
+                }
+            } catch (Exception e) {
+                // 예외 발생 시
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 삭제 중 오류 발생");
+            }
+        } else {
+            // fileUrl이 비어있는 경우
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("유효한 파일 경로가 없습니다.");
+        }
+
+
+
+    }
+
+
 }
--- src/main/java/com/example/attendance/service/CompanyFileService.java
+++ src/main/java/com/example/attendance/service/CompanyFileService.java
@@ -11,8 +11,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,12 +23,32 @@ public class CompanyFileService {
     private final CompanyFileRepository companyFileRepo;
 
 
-    public Page<CompanyFile> getCompanyFileList(int page){
+    public Page<CompanyFile> getCompanyFileList(int page) {
 
-            List<Sort.Order> sorts=new ArrayList<>();
-            sorts.add(Sort.Order.desc("createDate"));
-            Pageable pageable = PageRequest.of(page,10,Sort.by(sorts));
-            return this.companyFileRepo.findAll(pageable);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.companyFileRepo.findAll(pageable);
 
+    }
+
+    public void createCompanyFile(String fileName, String fileUrl) {
+        CompanyFile companyFile = new CompanyFile();
+        companyFile.setFileName(fileName);
+        companyFile.setFileUrl(fileUrl);
+        companyFile.setCreateDate(LocalDate.now());
+        this.companyFileRepo.save(companyFile);
+    }
+
+
+    public CompanyFile getCompanyFile(String id){
+        Optional<CompanyFile> temp= this.companyFileRepo.findById(id);
+        CompanyFile companyFile = temp.get();
+        return  companyFile;
+    }
+
+    public void companyFileDelete(CompanyFile companyFile){
+
+        this.companyFileRepo.deleteById(companyFile.getId());
     }
 }
